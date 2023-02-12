cadb5f1fc64cce5c133086b527b29183d061a98d
ZeekDaGeek <1709288+ZeekDaGeek@users.noreply.github.com>
2023-02-12 14:24:18 -0600

    sumneko_lua is depricated, changed to lua_ls

--- neovim-lua/plugin/lspconfig.lua
+++ neovim-lua/plugin/lspconfig.lua
@@ -84,7 +84,7 @@ nvim_lsp.tsserver.setup {
     capabilities = capabilities
 }
 
-nvim_lsp.sumneko_lua.setup {
+nvim_lsp.lua_ls.setup {
     capabilities = capabilities,
     on_attach = on_attach,
     settings = {
