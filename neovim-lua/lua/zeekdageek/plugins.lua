7f6ea4b2967a9504d91fc5cff688274d7af3d912
ZeekDaGeek <1709288+ZeekDaGeek@users.noreply.github.com>
2023-02-13 05:34:21 -0600

    attempt to fix vue lsp by only using null-ls

--- neovim-lua/lua/zeekdageek/plugins.lua
+++ neovim-lua/lua/zeekdageek/plugins.lua
@@ -137,7 +137,7 @@ return packer.startup({ function(use)
     }
     use {
         'nvim-telescope/telescope-fzf-native.nvim',
-        run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+        run = 'make'
     }
     use "nvim-telescope/telescope-file-browser.nvim"
 
